6a9e98678ece36f0d681b8ed56fb405afa08d583
Aniket Ghosh <ghoshaniket1000@gmail.com>
2024-08-25 17:09:13 +0530

    Implimented jwt token validation logic and protected get account route

--- api.go
+++ api.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 
+	jwt "github.com/golang-jwt/jwt/v5"
 	"github.com/gorilla/mux"
 )
 
@@ -26,7 +28,7 @@ func (s *ApiServer) Run() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/account", makeHttpHnadlerFunc(s.handleAccount))
-	router.HandleFunc("/account/{id}", makeHttpHnadlerFunc(s.handleGetAccount))
+	router.HandleFunc("/account/{id}", jwtAuth(makeHttpHnadlerFunc(s.handleGetAccount)))
 	router.HandleFunc("/transfer", makeHttpHnadlerFunc(s.handleTransfer))
 
 	log.Println("Api server is running on port: ", s.listenAddr)
@@ -100,6 +102,34 @@ func (s *ApiServer) handleTransfer(w http.ResponseWriter, r *http.Request) error
 	}
 	defer r.Body.Close()
 	return writeJson(w, http.StatusOK, transferreq)
+}
+
+func jwtAuth(handlerFunc http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tokenString := r.Header.Get("token")
+
+		_, err := validateJWT(tokenString)
+
+		if err != nil {
+			writeJson(w, http.StatusForbidden, ApiErr{Error: "Invalid token"})
+			return
+		}
+
+		handlerFunc(w, r)
+	}
+}
+
+func validateJWT(tokenString string) (*jwt.Token, error) {
+	secret := os.Getenv("JWT_SECRET")
+	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+		return []byte(secret), nil
+	})
 }
 
 // json writer
